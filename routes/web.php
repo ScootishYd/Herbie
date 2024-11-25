90827c8245361b92c1d4a0fc67bd6463428b49df
Yd <mail.ydpra@students.amikom.ac.id>
2024-11-25 10:12:54 +0700

    BE for Login

--- routes/web.php
+++ routes/web.php
@@ -1,26 +1,10 @@
 <?php
 
-<<<<<<< HEAD
-use App\Http\Controllers\ProfileController;
-=======
->>>>>>> 38e03ac (Set up a fresh Laravel app)
+use App\Http\Controllers\AutentikasiUser;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-<<<<<<< HEAD
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
-=======
->>>>>>> 38e03ac (Set up a fresh Laravel app)
